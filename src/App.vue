ad98a2d730e26146f81cb4bd3fa9e1ebe0b30994
lgmstudiosoftware <lgmstudiosoftware@gmail.com>
2025-05-31 21:56:59 +0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center p-10">
+    <h1 class="text-3xl font-bold text-blue-600">Vue + Tailwind + Docker</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
